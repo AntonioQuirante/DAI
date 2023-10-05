d61bf4cf5eed11b873a6d204b3afff6350e1fa91
Antonio Quirante <antonioqh.github@gmail.com>
2023-10-05 23:19:08 +0200

    P1 terminada

--- P1/Practica/e-commerce/Controllers/Controller.py
+++ P1/Practica/e-commerce/Controllers/Controller.py
@@ -3,6 +3,7 @@ import textwrap
 from pymongo import MongoClient
 import requests
 from pprint import pprint
+import os
 
 
 client = MongoClient('mongo', 27017)
@@ -21,9 +22,24 @@ def getProductos(api_url):
 def empty_database():
     productos_collection.drop()
 
+def download_image(url, id):
+    response = requests.get(url)
+    if response.status_code == 200:
+
+        file_name = f'img_{id}.jpg'
+        file_path = os.path.join('images', file_name)
+
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+
+        return file_path
+    else:
+        return None
+
 def insert(productos):
     for producto in productos:
         product = Product(**producto)
+        product.image = download_image(producto.get('image'), producto.get('id'))
         productos_collection.insert_one(product.model_dump())
 
 def find_between_price_range(min_price, max_price, category):
@@ -33,35 +49,35 @@ def find_between_price_range(min_price, max_price, category):
     }).sort("price", 1)
     return list(products)
 
-def find_products_with_keyword(keyword):
+def find_products(keyword):
     products = productos_collection.find({
         "description": {"$regex": keyword, "$options": "i"}
     })
     return list(products)
 
-def find_products_with_rating(min_rating):
+def products_rating(m_rating):
     products = productos_collection.find({
-        "rating.rate": {"$gt": min_rating}
+        "rating.rate": {"$gt": m_rating}
     })
     return list(products)
 
-def find_mens_clothing_sorted_by_rating():
+def mens_clothing_by_rating():
     products = productos_collection.find({
         "category": "men's clothing",
     }).sort("rating.rate", -1)
     return list(products)
 
-def calculate_total_revenue():
-    total_revenue = productos_collection.aggregate([
+def calculate_total():
+    total = productos_collection.aggregate([
         {"$group": {"_id": "_id", "total": {"$sum": "$price"}}}
     ])
-    return list(total_revenue)[0]["total"]
+    return list(total)[0]["total"]
 
-def calculate_revenue_by_category():
-    revenue_by_category = productos_collection.aggregate([
+def calculate_by_category():
+    revenue = productos_collection.aggregate([
         {"$group": {"_id": "$category", "total": {"$sum": "$price"}}}
     ])
-    return list(revenue_by_category)
+    return list(revenue)
 
 
 
