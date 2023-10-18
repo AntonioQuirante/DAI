112acd8496c527f0950bb334c4dd10dad206d979
Antonio Quirante <antonioqh.github@gmail.com>
2023-10-18 21:41:34 +0200

    practica terminada

--- P2/Practica/e-commerce/etienda/urls.py
+++ P2/Practica/e-commerce/etienda/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('get-productos/', views.getProductos, name='get_productos'),
+    path('get-productos/', views.insert, name='get_productos'),
     path('empty-database/', views.empty_database, name='empty_database'),
     path('find-products/<str:keyword>/', views.find_products, name='find_products'),
-    # Add more URL patterns for other views as needed
+    path('find-between-price-range/<int:min_price>/<int:max_price>/<str:category>/', views.find_between_price_range, name='find_between_price_range'),
+    path('products-rating/<int:m_rating>/', views.products_rating, name='products_rating'),
+    path('mens-clothing-by-rating/', views.mens_clothing_by_rating, name='mens_clothing_by_rating'),
+    path('calculate-total/', views.calculate_total, name='calculate_total'),
+    path('calculate-by-category/', views.calculate_by_category, name='calculate_by_category'),
 ]
--- P2/Practica/e-commerce/etienda/views.py
+++ P2/Practica/e-commerce/etienda/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-
+from bson import json_util
 from .models import Product
 import textwrap
 from pymongo import MongoClient
@@ -17,7 +17,7 @@ def index(request):
     return render(request, "index.html", context=None, content_type=None, status=None, using=None)
 
 
-def getProductos(api_url):
+def getProducts(api_url='https://fakestoreapi.com/products'):
     try:
         response = requests.get(api_url)
         response.raise_for_status()  # Lanza una excepción si la solicitud no es exitosa (código de estado no 2xx)
@@ -27,13 +27,19 @@ def getProductos(api_url):
         return None
 
 
-def empty_database():
+def empty_database(request):
     productos_collection.drop()
+    response_data = {
+        'message': 'Base de datos borrada'
+    }
+    return JsonResponse(response_data)
 
 
 def download_image(url, id):
     response = requests.get(url)
     if response.status_code == 200:
+
+        os.makedirs('images', exist_ok=True)
 
         file_name = f'img_{id}.jpg'
         file_path = os.path.join('images', file_name)
@@ -46,51 +52,84 @@ def download_image(url, id):
         return None
 
 
-def insert(productos):
-    for producto in productos:
+def insert(request):
+    products = getProducts()
+    for producto in products:
         product = Product(**producto)
         product.image = download_image(producto.get('image'), producto.get('id'))
         productos_collection.insert_one(product.model_dump())
+    response_data = {
+        'message': 'Datos Insertados'
+    }
+    return JsonResponse(response_data)
 
 
-def find_between_price_range(min_price, max_price, category):
+def find_between_price_range(request, min_price, max_price, category):
     products = productos_collection.find({
         "category": category,
         "price": {"$gte": min_price, "$lte": max_price}
     }).sort("price", 1)
-    return list(products)
+
+    product_list = list(products)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def find_products(keyword):
+def find_products(request, keyword):
     products = productos_collection.find({
         "description": {"$regex": keyword, "$options": "i"}
     })
-    return list(products)
+
+    product_list = list(products)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def products_rating(m_rating):
+def products_rating(request, m_rating):
     products = productos_collection.find({
         "rating.rate": {"$gt": m_rating}
     })
-    return list(products)
+    product_list = list(products)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def mens_clothing_by_rating():
+def mens_clothing_by_rating(request):
     products = productos_collection.find({
         "category": "men's clothing",
     }).sort("rating.rate", -1)
-    return list(products)
+
+    product_list = list(products)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def calculate_total():
+def calculate_total(request):
     total = productos_collection.aggregate([
         {"$group": {"_id": "_id", "total": {"$sum": "$price"}}}
     ])
-    return list(total)[0]["total"]
+    product_list = list(total)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
 
 
-def calculate_by_category():
+def calculate_by_category(request):
     revenue = productos_collection.aggregate([
         {"$group": {"_id": "$category", "total": {"$sum": "$price"}}}
     ])
-    return list(revenue)
+
+    product_list = list(revenue)
+    response_data = {
+        'message': json_util.dumps(product_list)
+    }
+    return JsonResponse(response_data, safe=False)
