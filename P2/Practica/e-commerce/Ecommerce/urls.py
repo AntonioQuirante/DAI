9e6b090e86a002082044f6eb867b48df01a724b6
Antonio Quirante <antonioqh.github@gmail.com>
2023-11-19 13:35:29 +0100

    empezando practica 2.4

--- P2/Practica/e-commerce/Ecommerce/urls.py
+++ P2/Practica/e-commerce/Ecommerce/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/etienda/")),
     path("etienda/", include("etienda.urls")),
     path("admin/", admin.site.urls),
 ]
