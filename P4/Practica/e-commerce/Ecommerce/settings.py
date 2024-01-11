e2c84cb67a10b6770d3efc4c1d0eb75ddfee9dee
Antonio Q-H <antonioqh@correo.ugr.es>
2024-01-11 21:12:39 +0100

    gunicorn and debug to false

--- P4/Practica/e-commerce/Ecommerce/settings.py
+++ P4/Practica/e-commerce/Ecommerce/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-72rvpnkprio4%n2s7ow1hil#v2sv@!bp5dzydmc&_-a7nk-3aa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- P4/Practica/frontend/src/components/SearchResultsList.jsx
+++ P4/Practica/frontend/src/components/SearchResultsList.jsx
@@ -1,12 +1,25 @@
+import React from "react";
 import "./SearchResultsList.css";
 import { SearchResult } from "./SearchResult";
 
 export const SearchResultsList = ({ results }) => {
+  const renderCardRows = () => {
+    return results.map((result, id) => (
+      <div key={id} className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{result.title}</h5>
+            <h5 className="card-title">{result.price}€</h5>
+            <h5 className="card-title">{result.description}</h5>
+          </div>
+        </div>
+      </div>
+    ));
+  };
+
   return (
-    <div className="results-list">
-      {results.map((result, id) => {
-        return <SearchResult result={result.title} key={id} />;
-      })}
+    <div className="row card-container">
+      {renderCardRows()}
     </div>
   );
 };
